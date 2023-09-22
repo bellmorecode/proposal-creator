d5bcabe81689392c7e990283eeca414f39430f9b
Glenn Ferrie <glenn@gfdata.io>
2023-09-22 10:14:24 -0400

    initial check-in with some connections to resources.

---
+++ propmaker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace propmaker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
